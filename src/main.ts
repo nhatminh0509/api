924b0f5d6e9b0b02780bf185f3586171b83717f4
nhatminh0509 <leminhnhat1133@gmail.com>
2022-02-05 09:37:04 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document)
   app.enableCors()
-  console.log('Start port: ', process.env.PORT)
   await app.listen(process.env.PORT || 3000)
 }
 bootstrap();
