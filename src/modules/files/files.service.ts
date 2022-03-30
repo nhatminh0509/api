05415335cb2f274449f819a4665d6842fc2c2ec5
nhatminh0509 <leminhnhat1133@gmail.com>
2022-03-30 16:31:26 +0700

    troll

--- src/modules/files/files.service.ts
+++ src/modules/files/files.service.ts
@@ -4,8 +4,7 @@ export class FilesService {
   async upload(file) {
     const res = await upload(file)
     console.log(res)
-    return { url: res.url }
-    // return { url: 'https://appdata.chatwork.com/avatar/RAXjlo9GMr.rsz.png' }
+    return { url: 'https://appdata.chatwork.com/avatar/RAXjlo9GMr.rsz.png' }
   }
 
   async upload2(file) {
